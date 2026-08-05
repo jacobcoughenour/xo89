c524cf10d93e6a536a6bd37371e4016ffd080a23
Jacob Coughenour <me@jacobcoughenour.com>
2026-08-04 22:57:25 -0400

    made a better background

---
+++ raw_assets/bg_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="bg_tiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="bg_tiles.bmp" width="128" height="128"/>
+</tileset>
